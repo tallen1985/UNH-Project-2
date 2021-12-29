dbb610d01209e047273fbf3ddf588aab1eede9f7
Gerard Kasemba <gerardkasemba@gmail.com>
2021-12-28 19:22:19 -0500

    ignored the package-lock

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize('booya', 'root', '',);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    'booya',
+    'root',
+    '',
     {
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
